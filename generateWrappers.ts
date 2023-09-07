cf45308e55840acdbf2ab5d8e836e828b4a3ac3a
David Rullán Díaz <drullan.github@gmail.com>
2023-09-07 02:54:53 +0200

    Update generateWrappers.ts

--- generateWrappers.ts
+++ generateWrappers.ts
@@ -1,7 +1,36 @@
 import * as fs from 'fs-extra';
 
 const components = [
-  // Aquí coloca la lista completa de componentes con name y parent
+  { "name": "Button", "parent": "IonButton", "interface": "ButtonInterface" },
+  { "name": "Header", "parent": "IonHeader", "interface": "HeaderInterface" },
+  { "name": "Footer", "parent": "IonFooter", "interface": "FooterInterface" },
+  { "name": "Title", "parent": "IonTitle", "interface": "TitleInterface" },
+  { "name": "Content", "parent": "IonContent", "interface": "ContentInterface" },
+  { "name": "List", "parent": "IonList", "interface": "ListInterface" },
+  { "name": "Item", "parent": "IonItem", "interface": "ItemInterface" },
+  { "name": "Input", "parent": "IonInput", "interface": "InputInterface" },
+  { "name": "Label", "parent": "IonLabel", "interface": "LabelInterface" },
+  { "name": "Checkbox", "parent": "IonCheckbox", "interface": "CheckboxInterface" },
+  { "name": "Radio", "parent": "IonRadio", "interface": "RadioInterface" },
+  { "name": "Select", "parent": "IonSelect", "interface": "SelectInterface" },
+  { "name": "Textarea", "parent": "IonTextarea", "interface": "TextareaInterface" },
+  { "name": "Icon", "parent": "IonIcon", "interface": "IconInterface" },
+  { "name": "Avatar", "parent": "IonAvatar", "interface": "AvatarInterface" },
+  { "name": "Card", "parent": "IonCard", "interface": "CardInterface" },
+  { "name": "Badge", "parent": "IonBadge", "interface": "BadgeInterface" },
+  { "name": "TabBar", "parent": "IonTabBar", "interface": "TabBarInterface" },
+  { "name": "TabButton", "parent": "IonTabButton", "interface": "TabButtonInterface" },
+  { "name": "Segment", "parent": "IonSegment", "interface": "SegmentInterface" },
+  { "name": "RouterOutlet", "parent": "IonRouterOutlet", "interface": "RouterOutletInterface" },
+  { "name": "Menu", "parent": "IonMenu", "interface": "MenuInterface" },
+  { "name": "Modal", "parent": "IonModal", "interface": "ModalInterface" },
+  { "name": "Toast", "parent": "IonToast", "interface": "ToastInterface" },
+  { "name": "Popover", "parent": "IonPopover", "interface": "PopoverInterface" },
+  { "name": "Loading", "parent": "IonLoading", "interface": "LoadingInterface" },
+  { "name": "Alert", "parent": "IonAlert", "interface": "AlertInterface" },
+  { "name": "ActionSheet", "parent": "IonActionSheet", "interface": "ActionSheetInterface" },
+  { "name": "ProgressBar", "parent": "IonProgressBar", "interface": "ProgressBarInterface" },
+  { "name": "Spinner", "parent": "IonSpinner", "interface": "SpinnerInterface" }
 ];
 
 // Ruta donde se generarán las carpetas y archivos
